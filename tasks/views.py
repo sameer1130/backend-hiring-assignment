0cdf881727329d95f5da1e747dd481521475c4ac
Sameer Kamble <sameerkamble1130@gmail.com>
2025-03-07 18:53:57 +0530

    Created generic views in view.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProjectSerializer,TaskSerializer
+from .models import Project,Task
+
+
 
 # Create your views here.
+
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    serializer = ProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.filter(end_date__isnull=True)
+
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.object.all()
+    serializer = TaskSerializer
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
