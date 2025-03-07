27c2c008d1f22938ac4dcb1223ab8e8838a031a2
Sameer Kamble <sameerkamble1130@gmail.com>
2025-03-07 18:29:48 +0530

    Generated Serializers for task and project

---
+++ tasks/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Project, Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class ProjectSerializzer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many = True, read_only = True)
+    class Meta:
+        model = Project
+        fields = '__all__'
